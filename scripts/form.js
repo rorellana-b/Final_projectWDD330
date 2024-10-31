98148d86fe38d3fcf04d593e2835241d90548511
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.2>
2024-10-30 20:48:04 -0600

    form and about us detail, added localstorage

---
+++ scripts/form.js
@@ -0,0 +1,44 @@
+const registrationForm = document.getElementById('registration-form');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('close-modal');
+const welcomeMessage = document.getElementById('welcome-message');
+
+registrationForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    // Saved user at localstorage
+    const userData = {
+        username: username,
+        email: email,
+        password: password,
+    };
+
+    localStorage.setItem('user', JSON.stringify(userData));
+
+    welcomeMessage.innerText = `¡Listo! Bienvenido a nuestra comunidad, ${username}`;
+    modal.style.display = 'block';
+    registrationForm.reset();
+});
+
+closeModal.onclick = function () {
+    modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
+
+// Show a welcome message
+window.onload = () => {
+    const savedUser = JSON.parse(localStorage.getItem('user'));
+    if (savedUser) {
+        welcomeMessage.innerText = `Hi, ${savedUser.username}! Thanks for join us.`;
+        modal.style.display = 'block';
+    }
+};
